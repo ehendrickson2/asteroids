cdb49549721ffb16de2abed5410c618c67f6a249
ehendrickson2 <edwinhendrickson2@gmail.com>
2025-04-06 15:34:27 -0500

    Added player ship and movement, added asteroids and asteroid field spawning objects

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
+    # Initialization
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
-    screen.fill((0, 0, 0))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    # Instantiate objects after assigning to groups/containers
+    player = Player(x, y)
+    AsteroidField()
+
+    # Start Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Set black background
+        screen.fill((0, 0, 0))
+
+        updatable.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+
+        clock.tick(60)
+        dt = clock.get_time() / 1000
         pygame.display.flip()
 
 
