862a50477b72dc3ea8ec08c58ba9112443760346
ehendrickson2 <edwinhendrickson2@gmail.com>
2025-04-06 21:17:51 -0500

    Added background, player ship image, and added acceleration to movement

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 from circleshape import *
 from tools import Tools
@@ -6,9 +7,11 @@ from constants import (
     PLAYER_RADIUS,
     PLAYER_TURN_SPEED,
     PLAYER_SPEED,
+    PLAYER_THRUST_ACCEL,
     SHOT_RADIUS,
     PLAYER_SHOOT_SPEED,
     PLAYER_SHOOT_COOLDOWN,
+    BASE_PATH,
 )
 
 
@@ -16,11 +19,21 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.velocity = pygame.Vector2(0, 0)
         self.shoot_timer = 0
         self.score = 0
+        self.image = pygame.image.load(
+            os.path.join(BASE_PATH, "player_ship.png")
+        ).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (45, 45))  # optional resize
+        self.image = pygame.transform.rotate(self.image, 180)
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        rotated_image = pygame.transform.rotate(self.image, -self.rotation)
+        rect = rotated_image.get_rect(center=self.position)
+
+        screen.blit(rotated_image, rect.topleft)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -33,9 +46,9 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
-    def move(self, dt):
+    def move(self, acceleration, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        self.velocity += forward * acceleration * dt
 
     def shoot(self):
         bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
@@ -48,6 +61,9 @@ class Player(CircleShape):
         Tools.update_text(self.score)
 
     def update(self, dt):
+        self.velocity *= 0.99  # friction coefficent
+        self.position += self.velocity * dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -55,9 +71,9 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             self.rotate(dt)
         if keys[pygame.K_w]:
-            self.move(dt)
+            self.move(PLAYER_THRUST_ACCEL, dt)
         if keys[pygame.K_s]:
-            self.move(-dt)
+            self.move(-PLAYER_THRUST_ACCEL, dt)
         if keys[pygame.K_SPACE] and self.shoot_timer <= 0:
             self.shoot()
             self.shoot_timer = PLAYER_SHOOT_COOLDOWN
